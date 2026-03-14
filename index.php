bb1fa61c6b710f5c6a625e5ca043105ce8928990
GustaAC <gustavoalcantaracraiba@gmail.com>
2026-03-13 23:44:43 -0300

    Reenvio]

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mensagem .= "Erro: o nome de usuário deve ter entre 4 e 15 caracteres.<br>";
         $erroTamanho = true;
     }
+
  
       if (strlen($senha) < 4 || strlen($senha) > 15) {
         $mensagem .= "Erro: a senha deve ter entre 4 e 15 caracteres.<br>";
@@ -19,17 +20,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       }
 
   if ($erroTamanho == false) {
+
       if (($usuario == "PROFESSOR" || $usuario == "COORDENADOR") && $senha == "DEVISATE") {
             $data = date("d/m/Y");
              $hora = date("H:i");
              $mensagem = "Bem-vindo, $usuario! Você realizou acesso às $hora no dia $data.";
-        } elseif ($usuario != "PROFESSOR" && $usuario != "COORDENADOR" && $senha != "DEVISATE") {
+         } elseif ($usuario != "PROFESSOR" && $usuario != "COORDENADOR" && $senha != "DEVISATE") {
             $mensagem = "Erro: nome de usuário e senha inválidos.";
          } elseif ($usuario != "PROFESSOR" && $usuario != "COORDENADOR") {
-            $mensagem = "Erro: nome de usuário inválido.";
-        } else {
+       $mensagem = "Erro: nome de usuário inválido.";
+         } else {
             $mensagem = "Erro: senha incorreta.";
         }
+
      } 
 }
 
@@ -40,20 +43,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html>
  <head>
-    <title>Login</title>
+    <title>Login Comercial</title>
 </head>
 <body>
 
     <form method="POST">
-         <label>Usuário:</label>
+
+         <label>Usuário: </label>
         <input type="text" name="usuario" required>
+
+
            <br>
         <label>Senha: </label>
         <input type="password" name="senha" required>
+
             <br>
          <button type="submit">Entrar</button>
     </form>
-    
+
     <p><b><?php echo $mensagem; ?></b></p> 
 
 </body>
